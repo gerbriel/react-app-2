a6d79e5f13d02f50948ea8f530501fb5c5f47403
gerbriel <gabrielriosemail@gmail.com>
2021-02-24 13:06:33 -0800

    update product list
    Got products to display as a grid 2x2 with content and button below

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 //product-list
-
 import React, {Component} from 'react'
 import './App.css';
+import data from './data.json';
+import Products from './components/Products/Products';
 
 
-function App(){
-  return(
-    <div className='grid-container'>
-      <header>
-        <a heref='/'>React store</a>
-      </header>
-      <main>
-        product list
-      </main>
-      <footer>
-        all rights reserved
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      products: data.products,
+      size:"",
+      sort:"",
+    };
+  }
+
+  render() {
+    return(
+      <div className='grid-container'>
+        <header>
+          <a heref='/'>React store</a>
+        </header>
+        <main>
+          <div className='content'>
+            <div className='main'>
+              <Products products={this.state.products}/>
+            </div>
+            <div className='sidebar'>
+              cart items
+            </div>
+          </div>
+        </main>
+        <footer>
+          all rights reserved
+        </footer>
+      </div>
+    )};
+  
 }
 
 export default App;
